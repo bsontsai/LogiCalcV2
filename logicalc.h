d321d854aa7a211862bab28a0c8be0a4290a6975
Benson Tsai <nikejiman3@gmail.com>
2024-02-22 05:37:10 -0500

    full version

---
+++ logicalc.h
@@ -0,0 +1,49 @@
+#ifndef LOGICALC_H
+#define LOGICALC_H
+
+#include <stdbool.h>
+
+/* Global variables */
+
+extern int g_variable_count;
+extern char *g_variable_names[20];
+extern bool *g_variable_address[20];
+
+/* Constant definitions */
+#define MAX_STRING_LEN (50)
+#define NUM_OPS        (7)
+
+/* Error codes */
+#define OK             (-1)
+#define BAD_INPUT      (-2)
+#define OUTPUT_ERROR   (-3)
+
+typedef enum {
+  END = -1,
+  VAR = 0,
+  NOT = 1,
+  AND = 2,
+  XOR = 3,
+  OR = 4,
+  IMPL = 5,
+  BICOND = 6
+} operation;
+
+
+/* Structure declaration */
+typedef struct proposition {
+  operation o;
+  bool value;
+  struct proposition *a;
+  struct proposition *b;
+  char *name;
+} proposition;
+
+
+/* prototype */
+
+void solve(proposition *);
+
+
+
+#endif //LOGICALC_H
